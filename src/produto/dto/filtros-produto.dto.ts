214d7ea018e65c2038ae2a2bf3463b63174343cf
vitormedeiros911 <vitormedeiros911@outlook.com>
2024-06-16 22:52:53 -0300

    chore: Alteração na paginação de produtos

--- src/produto/dto/filtros-produto.dto.ts
+++ src/produto/dto/filtros-produto.dto.ts
@@ -1,19 +1,28 @@
 import { ApiPropertyOptional } from '@nestjs/swagger';
-import { IsNotEmpty, IsNumberString, IsString } from 'class-validator';
+import {
+  IsDefined,
+  IsNotEmpty,
+  IsNumberString,
+  IsOptional,
+  IsString,
+} from 'class-validator';
 
 export class FiltrosProdutoDto {
   @IsString()
   @IsNotEmpty()
+  @IsOptional()
   @ApiPropertyOptional()
   nome: string;
 
   @IsNumberString()
   @IsNotEmpty()
+  @IsOptional()
   @ApiPropertyOptional()
   skip: number;
 
   @IsNumberString()
   @IsNotEmpty()
-  @ApiPropertyOptional()
+  @IsDefined()
+  @ApiPropertyOptional({ default: 10 })
   limit: number;
 }

bf46e56a78ef10a038edf8a0a224f3a72a88cb4e
vitormedeiros911 <vitormedeiros911@outlook.com>
2024-06-16 22:18:09 -0300

    feat: Adiciona inativação e ativação de produto

--- src/produto/produto.controller.ts
+++ src/produto/produto.controller.ts
@@ -18,6 +18,7 @@ import { GetUsuario } from '../shared/decorators/get-user.decorator';
 import { Perfis } from '../shared/decorators/perfis.decorator';
 import { PerfilEnum } from '../shared/enum/perfil.enum';
 import { PerfisGuard } from '../shared/guards/perfil.guard';
+import { StatusEnum } from '../shared/interfaces/status.enum';
 import { IUsuario } from '../shared/interfaces/usuario.interface';
 import { FiltrosProdutoDto } from './dto/filtros-produto.dto';
 import { ProdutoDto } from './dto/produto.dto';
@@ -55,7 +56,13 @@ export class ProdutoController {
   @Get(':id')
   @ApiOperation({ summary: 'Buscar produto por id' })
   async buscarProdutoPorId(@Param('id') id: string) {
-    return this.clientProdutoBackend.send('buscar-produto-por-id', id);
+    return this.clientProdutoBackend
+      .send('buscar-produto-por-id', id)
+      .pipe(
+        catchError((error) =>
+          throwError(() => new HttpException(error.response, error.status)),
+        ),
+      );
   }
 
   @Put(':id')
@@ -74,6 +81,48 @@ export class ProdutoController {
           ...produtoDto,
           idFarmacia: usuario.idFarmacia,
         },
+      })
+      .pipe(
+        catchError((error) =>
+          throwError(() => new HttpException(error.response, error.status)),
+        ),
+      );
+  }
+
+  @Put(':id/ativar')
+  @UseGuards(PerfisGuard)
+  @Perfis([PerfilEnum.ADMIN_FARMACIA])
+  @ApiOperation({ summary: 'Ativar produto' })
+  async ativarProduto(
+    @Param('id') id: string,
+    @GetUsuario() usuario: IUsuario,
+  ) {
+    return this.clientProdutoBackend
+      .send('atualizar-produto', {
+        id,
+        idFarmacia: usuario.idFarmacia,
+        status: StatusEnum.ATIVO,
+      })
+      .pipe(
+        catchError((error) =>
+          throwError(() => new HttpException(error.response, error.status)),
+        ),
+      );
+  }
+
+  @Put(':id/inativar')
+  @UseGuards(PerfisGuard)
+  @Perfis([PerfilEnum.ADMIN_FARMACIA])
+  @ApiOperation({ summary: 'Inativar produto' })
+  async inativarProduto(
+    @Param('id') id: string,
+    @GetUsuario() usuario: IUsuario,
+  ) {
+    return this.clientProdutoBackend
+      .send('atualizar-produto', {
+        id,
+        idFarmacia: usuario.idFarmacia,
+        status: StatusEnum.INATIVO,
       })
       .pipe(
         catchError((error) =>

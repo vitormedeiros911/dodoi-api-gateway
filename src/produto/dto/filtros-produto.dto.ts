a0e49908f3b7f28f49749bfabaaa226cc4654cc4
vitormedeiros911 <vitormedeiros911@outlook.com>
2024-11-29 00:43:37 -0300

    chore: Adiciona ordenação na filtragem de produtos

--- src/produto/dto/filtros-produto.dto.ts
+++ src/produto/dto/filtros-produto.dto.ts
@@ -1,8 +1,13 @@
-import { ApiPropertyOptional } from '@nestjs/swagger';
+import { ApiProperty, ApiPropertyOptional } from '@nestjs/swagger';
 import { IsEnum, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 import { PaginationDto } from '../../shared/dto/pagination.dto';
 import { StatusEnum } from '../../shared/enum/status.enum';
+import { OrderEnum } from '../../shared/enum/order.enum';
+
+enum OrderByProdutoEnum {
+  'createdAt',
+}
 
 export class FiltrosProdutoDto extends PaginationDto {
   @IsString()
@@ -16,4 +21,18 @@ export class FiltrosProdutoDto extends PaginationDto {
   @IsEnum(StatusEnum, { each: true })
   @ApiPropertyOptional({ enum: StatusEnum, isArray: true })
   status: string[];
+
+  @ApiProperty({ required: false, enum: OrderByProdutoEnum, isArray: false })
+  @IsString()
+  @IsEnum(OrderByProdutoEnum)
+  orderBy: string;
+
+  @ApiProperty({ required: false, enum: OrderEnum, isArray: false })
+  @IsEnum(OrderEnum)
+  order: string;
+
+  @ApiProperty({ required: false })
+  @IsOptional()
+  @IsString()
+  idFarmacia: string;
 }
